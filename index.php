046cdad2c0bcc8fafef4d6e8b1f5f85adfc32d80
jekyllx <anand.pal2050@gmail.com>
2017-10-28 00:11:48 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Shopper - online E-commerce website</title>
+
+<!-- Css Files  -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  </head>
+  <body>
+
+<!--================
+      Navigation
+====================  -->
+<nav class="navbar navbar-expand-lg">
+  <a class=" navbar-brand" href="index.php"><strong>SHOPPER</strong></a>
+  <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar" aria-controls="#navbar" aria-label="toggle-navigation">
+    <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="drop-link" data-toggle="dropdown">
+          electronics
+        </a>
+        <div class="dropdown-menu" id="dropdown">
+          <a class="dropdown-item" href="#">mobiles</a>
+          <a class="dropdown-item" href="#">home entertainments</a>
+          <a class="dropdown-item" href="#">laptops</a>
+          <a class="dropdown-item" href="#">televisions</a>
+        </div>
+
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="drop-link" data-toggle="dropdown">
+          appliances
+        </a>
+        <div class="dropdown-menu" id="dropdown">
+          <a class="dropdown-item" href="#">mobiles</a>
+          <a class="dropdown-item" href="#">laptop</a>
+          <a class="dropdown-item" href="#">tv</a>
+          <a class="dropdown-item" href="#">speakers</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="drop-link" data-toggle="dropdown">
+          men
+        </a>
+        <div class="dropdown-menu" id="dropdown">
+          <a class="dropdown-item" href="#">mobiles</a>
+          <a class="dropdown-item" href="#">laptop</a>
+          <a class="dropdown-item" href="#">tv</a>
+          <a class="dropdown-item" href="#">speakers</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="drop-link" data-toggle="dropdown">
+          women
+        </a>
+        <div class="dropdown-menu" id="dropdown">
+          <a class="dropdown-item" href="#">mobiles</a>
+          <a class="dropdown-item" href="#">laptop</a>
+          <a class="dropdown-item" href="#">tv</a>
+          <a class="dropdown-item" href="#">speakers</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="drop-link" data-toggle="dropdown">
+          books
+        </a>
+        <div class="dropdown-menu" id="dropdown">
+          <a class="dropdown-item" href="#">mobiles</a>
+          <a class="dropdown-item" href="#">laptop</a>
+          <a class="dropdown-item" href="#">tv</a>
+          <a class="dropdown-item" href="#">speakers</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="drop-link" data-toggle="dropdown">
+          offers
+        </a>
+        <div class="dropdown-menu" id="dropdown">
+          <a class="dropdown-item" href="#">mobiles</a>
+          <a class="dropdown-item" href="#">laptop</a>
+          <a class="dropdown-item" href="#">tv</a>
+          <a class="dropdown-item" href="#">speakers</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline" id="search">
+      <div class="input-group">
+      <input class="form-control" type="text" name="search" placeholder="search...">
+      <button class="btn btn-light input-group-addon" type="button" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+      </div>
+    </form>
+    <form class="form-inline" id="users">
+      <ul>
+        <li>
+          <i class="fa fa-user-circle" aria-hidden="true"></i>
+        </li>
+        <li>
+          <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+        </li>
+      </ul>
+
+    </form>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Javascripts Files -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+  </body>
+</html>
